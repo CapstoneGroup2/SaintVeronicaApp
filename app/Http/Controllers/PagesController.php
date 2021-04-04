38301449beab5552d635ba98b09582d2df2c54b6
Josephine Morre <josephine.morre@student.passerellesnumeriques.org>
2021-04-04 11:11:36 +0800

    dashboard updates

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -75,9 +75,9 @@ class PagesController extends Controller
         }
 
         $data_enrollees_per_month = DB::table('students')
-        // ->select(
-                                                            // DB::raw('count(*) as number'),
-                                                            // DB::raw('month(created_at) as month_name'))
+        ->select(
+                                                            DB::raw('count(*) as number'),
+                                                            DB::raw('month(created_at) as month_name'))
                                                             // DB::raw('max(created_at) as created_at'),
                                                         // ->whereYear('created_at', '=', 2021)
                                                         ->groupBy('month_name')

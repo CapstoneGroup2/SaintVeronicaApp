c7cc72ab20ca98c79f7c17f893d534e67b310f46
josephine-pn3ed <josephine.morre@student.passerellesnumeriques.org>
2021-05-17 10:36:51 +0800

    updated

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,6 @@ Route::get('/login', 'Auth\MainController@index');
 
 Route::post('/login', 'Auth\MainController@checklogin');
 
-Auth::routes(['verify' => true]);
-
 Route::middleware(['admin'])->group(function () {
 
     Route::get('/', function() {
